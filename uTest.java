dc099ef06b46fdf18e0070d8bae225b879779625
Bjørn Utzon <nnn.utzon@hotmail.com>
2021-03-15 16:55:36 +0000

    now it doesn't complain

--- uTest.java
+++ uTest.java
@@ -94,46 +94,45 @@ public class uTest {
 //Circle TEST
 @Test
     public void testCircArea() {
-        Circle circ1 = new Circle(10, 10, 20, 20, 5, 10);
+        Circle Circ1 = new Circle(10, 10, 20, 20, 5, 10);
 
         // double expected, double actual, double delta
-        assertEquals(3, Tri1.area(), 0);
+        assertEquals(628.3185307179588, Circ1.area(), 0);
     }
     @Test
     public void testCircCircumference() {
-        Circle circ1 = new Circle(10, 10, 20, 20, 5, 10);
+        Circle Circ1 = new Circle(10, 10, 20, 20, 5, 10);
         
         // double expected, double actual, double delta
-        assertEquals(8.60555127546399, Tri1.circumference(), 0);
+        assertEquals(88.85765876316732, Circ1.circumference(), 0);
     }
 
     //tests the center coordinate x
 
     @Test
     public void testCircCenterX() {
-        Circle circ1 = new Circle(10, 10, 20, 20, 5, 10);
+        Circle Circ1 = new Circle(10, 10, 20, 20, 5, 10);
 
         // double expected, double actual, double delta
-        assertEquals(2.3333333333333335, Tri1.centerx(), 0);
+        assertEquals(10.0, Circ1.centerx(), 0);
     }
 
     //tests the center coordinate y
     @Test
     public void testCircCenterY() {
-        Triangle Tri1 = new Triangle(1, 1, 3, 3, 5, 5);
+        Circle Circ1 = new Circle(10, 10, 20, 20, 5, 10);
 
         // double expected, double actual, double delta
-        assertEquals(2.0, Tri1.centery(), 0);
+        assertEquals(10.0, Circ1.centery(), 0);
     } 
 
     @Test
     public void testCircIsInside () {
-        Triangle Tri1 = new Triangle(1, 1, 3, 3, 5, 5);
+        Circle Circ1 = new Circle(10, 10, 20, 20, 5, 10);
     
         // double expected, double actual
-        assertEquals(false, Tri1.isInside());
-
-
+        assertEquals(false, Circ1.isInside());
+	}
 
 
 }
